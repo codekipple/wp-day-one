be29610b841f88daa877d0ef87126a454e647cd0
codekipple <codekipple@gmail.com>
2015-11-13 16:47:27 +0000

    Added sample config.

---
+++ web/wp-config-sample.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+define('WEB_DIR', dirname(__FILE__));
+define('VENDORS_DIR', realpath(WEB_DIR . '/../vendor'));
+
+// We use server vars wherever possible, but absolute paths are provided for the benefit of CLI
+
+if (!strstr(__DIR__, '.')) {
+  define('WP_ENV', 'dev');
+  define('DB_PASSWORD', 'password');
+  define('DB_NAME', 'change_me');
+  define('DB_USER', 'change_me');
+  define('WP_DEBUG', true);
+  define('SAVEQUERIES', true);
+} else if (strstr(__DIR__, 'demo') || strstr(__DIR__, 'staging')) {
+  define('WP_ENV', 'demo');
+  define('DB_PASSWORD', 'change_me');
+  define('DB_NAME', 'change_me');
+  define('DB_USER', 'change_me');
+  define('WP_DEBUG', false);
+} else {
+  define('WP_ENV', 'live');
+  define('DB_PASSWORD', 'change_me');
+  define('DB_NAME', 'change_me');
+  define('DB_USER', 'change_me');
+  define('WP_DEBUG', false);
+}
+
+defined('DB_HOST') or define('DB_HOST', 'localhost');
+
+defined('WP_HOME') or define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', WP_HOME . '/wordpress');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/** tell wordpress where the wp-content file has been moved to */
+define('WP_CONTENT_DIR', dirname(__FILE__) . '/content');
+define('WP_CONTENT_URL', WP_HOME .'/content');
+define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins');
+define('WP_PLUGIN_URL', WP_CONTENT_URL .'/plugins');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/*
+  Symfony2 Forms CRSF Token
+  To secure your application against CSRF attacks, you need to define a CSRF secret. Generate a random string with at least 32 characters, insert it in the above snippet and make sure that nobody except your web server can access the secret.
+ */
+define('FORMS_CRSF_TOKEN', 'put your unique phrase here');
+
+/**
+ * limit post revisions to 10 to avoid database bloat
+ * default is 25
+ */
+define('WP_POST_REVISIONS', 10);
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define ('WPLANG', 'en_GB');
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if (!defined('ABSPATH')) {
+  define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
